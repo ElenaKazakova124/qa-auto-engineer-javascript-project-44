0ea994585ba76153e6e996453d4523a6fac30fc3
Elena <elena.kazakova124@gmail.com>
2025-04-26 00:09:42 +0300

    calc_game

---
+++ src/games/calc.js
@@ -0,0 +1,17 @@
+import runGame from '../index.js';
+import { getRandomNumber } from '../helper.js';
+import { getRandomOperator } from '../helper.js';
+
+const description = 'What is the result of the expression?'
+
+const generateRound = () => {
+  const digit_first = getRandomNumber(1, 100);
+  const digit_second = getRandomNumber(1, 100);
+  const operator = getRandomOperator();
+  const question = `${digit_first} ${operator} ${digit_second}`;
+  
+  const correctAnswer = eval(question);
+  return [question.toString(), correctAnswer.toString()];
+};
+
+export default () => runGame(description, generateRound);
--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,11 @@
 export const getRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
+
+  export const getRandomOperator = () => {
+    {
+      const operators = ['+', '-', '*', '/']
+      const randomIndex = Math.floor(Math.random() * operators.length);
+      return operators[randomIndex];
+    }
+  }
